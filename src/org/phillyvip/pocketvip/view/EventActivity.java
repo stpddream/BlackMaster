4b93d7961c75a69ebb5b17043966ac8d02e2174f
cliu <stmishu@gmail.com>
2013-03-04 16:45:17 -0500

    qqqqqqq

---
+++ src/org/phillyvip/pocketvip/view/EventActivity.java
@@ -0,0 +1,13 @@
+package org.phillyvip.pocketvip.view;
+
+import org.phillyvip.pocketvip.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EventActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event);
+	}
+}
